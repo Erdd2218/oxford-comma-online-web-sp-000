f0ebef3597134e8e4b493bca352ba520efb0484b
Andre Pierre <andre.laurence94@gmail.com>
2021-06-17 03:30:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,7 @@
+
 def oxford_comma(array)
-   case array.length 
- when 1
-   "#{array[0]}"
- when 2
-   array[0..1].join(" and ")
- else 
-   array[0...-1].join(", ") << ", and #{array[-1]}"
- end 
- end 
+  last_ele = array.pop()
+  new_arr = array.join( ", ") << ", and " + last_ele
+  return new_arr
+end
+
